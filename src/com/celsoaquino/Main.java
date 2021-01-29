e4d8f22588d16221599c915fd90e226ffb434c48
celsoaquino <celso.aquino@ymail.com>
2021-01-29 12:23:05 -0300

    First class: Position

--- src/com/celsoaquino/Main.java
+++ src/com/celsoaquino/Main.java
@@ -1,8 +1,12 @@
 package com.celsoaquino;
 
+import com.celsoaquino.boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
