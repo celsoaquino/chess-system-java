145af5c638a60581aca05145cb75cdf396e30b13
celsoaquino <celso.aquino@ymail.com>
2021-02-02 11:11:24 -0300

    Moving pieces

--- src/com/celsoaquino/chess/ChessException.java
+++ src/com/celsoaquino/chess/ChessException.java
@@ -1,6 +1,8 @@
 package com.celsoaquino.chess;
 
-public class ChessException extends RuntimeException {
+import com.celsoaquino.boardgame.BoardException;
+
+public class ChessException extends BoardException {
 
     public ChessException(String message) {
         super(message);
